ecd0a52a994d97d8df388686308c7d0a85558ea8
Mike Tipper <mike@tailoredsoftwaresolutions.com.au>
2023-03-20 16:22:41 +1100

    Fetch The Games

---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: '2e681081e5b746d29ff5e582f186bf8f', // Key will be added in the query string to each API call
+  },
+});
