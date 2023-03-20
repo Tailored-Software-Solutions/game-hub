f4205c4484d68b91952dea0ceb0a7c552c7e605b
Mike Tipper <mike@tailoredsoftwaresolutions.com.au>
2023-03-20 16:34:44 +1100

    Create Custom Games Hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
@@ -6,3 +6,5 @@ export default axios.create({
     key: '2e681081e5b746d29ff5e582f186bf8f', // Key will be added in the query string to each API call
   },
 });
+
+export { CanceledError };
